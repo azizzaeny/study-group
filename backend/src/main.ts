92c615c5d9483a39143ac43937baca149a9f7150
azizzaeny <zie.azlance@gmail.com>
2020-10-19 12:19:03 +0700

    change cors setting at main bootstrap

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,24 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { TimeoutInterceptor } from 'components/interceptors/timeout.interceptor';
-import { HttpExceptionFilter } from 'components/filters/http-exception.filter';
+//import { TimeoutInterceptor } from 'components/interceptors/timeout.interceptor';
+import { HttpExceptionFilter } from './components/filters/http-exception.filter';
 
 
 async function bootstrapHttp() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        cors: true,
+    });
 
     app.enableShutdownHooks();
     //app.useLogger
     app.useGlobalFilters(new HttpExceptionFilter());
-    app.useGlobalInterceptors(new TimeoutInterceptor());
+    // app.useGlobalInterceptors(new TimeoutInterceptor());
     app.useGlobalPipes(
         new ValidationPipe({
             transform: true
         }));
-    app.enableCors({ credentials: true });
-    app.setGlobalPrefix('api');
-    await app.listen(3000, _ => console.log('Http server up and running on port 3000'));
+
+    //app.enableCors({ credentials: true });
+
+    //app.setGlobalPrefix('api');
+
+    await app.listen(3000);
+    console.log('Http server up and running on port 3000')
     return app;
 }
 
