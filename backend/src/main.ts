809bb7e46db23edce677a905f9fa65d146e3e804
azizzaeny <zie.azlance@gmail.com>
2020-10-19 10:37:21 +0700

    add global prefix, use filters, interceptors, and pipes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { TimeoutInterceptor } from 'components/interceptor/timeout.interceptor';
+import { HttpExceptionFilter } from 'components/filters/http-exception.filter';
+
 
 async function bootstrapHttp() {
     const app = await NestFactory.create(AppModule);
+
     app.enableShutdownHooks();
-    await app.listen(3000);
+    //app.useLogger
+    app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalInterceptors(new TimeoutInterceptor());
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true
+        }));
+    app.enableCors({ credentials: true });
+    app.setGlobalPrefix('api');
+    await app.listen(3000, _ => console.log('Http server up and running on port 3000'));
     return app;
 }
 
