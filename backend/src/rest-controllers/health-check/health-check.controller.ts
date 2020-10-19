80083300c51bda089791c9991312c2f6f202805c
azizzaeny <zie.azlance@gmail.com>
2020-10-19 12:17:26 +0700

    added rest-controller helath-check module

---
+++ backend/src/rest-controllers/health-check/health-check.controller.ts
@@ -0,0 +1,17 @@
+import { Get, Controller } from '@nestjs/common';
+
+@Controller('health-check')
+export class HealthCheckController {
+    private upTime: number;
+    constructor() {
+        this.upTime = Date.now();
+    }
+    @Get('healthcheck')
+    async get() {
+        const now = Date.now();
+        return {
+            status: 'API Online',
+            uptime: Number((now - this.upTime) / 1000).toFixed(0)
+        };
+    }
+}
