902fbc057db03123fd209b73265c168fdd97558d
azizzaeny <zie.azlance@gmail.com>
2020-10-21 00:12:52 +0700

    add user interface and user schema declaration

---
+++ backend/src/module/user/user.interface.ts
@@ -0,0 +1,7 @@
+import { Document, Model, Schema } from "mongoose";
+import { IUser, user_schema } from './../../domain/user';
+
+export interface IUserDocument extends IUser, Document { };
+export interface IUserModel extends Model<IUserDocument> { };
+
+export const UserSchema = new Schema(user_schema());
