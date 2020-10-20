a39a7ad845fea7d3b3a8bfa9f62462308e8e1690
azizzaeny <zie.azlance@gmail.com>
2020-10-20 22:59:17 +0700

    import mongooseModule, connect from db_url

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import config from './config';
+import { db_url } from './domain/db';
+
+const conf = config();
 
 const configModule = ConfigModule.forRoot({
   load: [config]
 });
 
+const mongooseModule = MongooseModule.forRoot(db_url(conf.db));
+
 @Module({
-  imports: [configModule],
+  imports: [configModule, mongooseModule],
   controllers: [AppController],
   providers: [AppService]
 })
