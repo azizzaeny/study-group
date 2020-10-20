60a1b7f4b057eee5b65bf2dabfe6ae8b313dd23e
azizzaeny <zie.azlance@gmail.com>
2020-10-21 00:13:25 +0700

    register user schema declaration to mongoose

--- backend/src/module/user/user.module.ts
+++ backend/src/module/user/user.module.ts
@@ -1,10 +1,15 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserSchema } from './user.interface';
+import { user_feature } from './../../domain/user';
+
+const mongooseFeature = MongooseModule.forFeature(user_feature(UserSchema));
 
 @Module({
-  imports: [],
+  imports: [mongooseFeature],
   controllers: [UserController],
   providers: [UserService]
 })
