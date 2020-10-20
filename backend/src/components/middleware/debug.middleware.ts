a44f3fab7b9219fd947e3dc23802ad2d1d701bb9
azizzaeny <zie.azlance@gmail.com>
2020-10-20 22:00:29 +0700

    major refactor, delete old unused files

--- backend/src/components/middleware/debug.middleware.ts
+++
@@ -1,6 +0,0 @@
-import { Logger } from '@nestjs/common';
-
-export function debugRequest(req, res, next) {
-    Logger.debug(` ${req.headers['user-agent'].split(') ')[0]}`, 'Request initated', false);
-    next();
-}
--- backend/src/core/return.ts
+++
@@ -1,7 +0,0 @@
-
-export function docs_referer(): { message: string, link: string } {
-    return {
-        message: 'To use this services Please read documentation @github/azizzaeny/study-group',
-        link: 'https://github.com/azizzaeny/study-group/'
-    };
-}
