37894fc8554780cb44a8de025228511dcc02aa6d
azizzaeny <zie.azlance@gmail.com>
2020-10-21 07:11:15 +0700

    add exception filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-
+import { DefaultExceptionFilter } from './common/filters/default-exception.filter';
 
 import { AppModule } from './app.module'
 import config from './config';
@@ -9,6 +9,9 @@ const conf = config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalFilters(new DefaultExceptionFilter());
+
   await app.listen(conf.http.port);
 
   console.log('Http Application started at port ', conf.http.port);
