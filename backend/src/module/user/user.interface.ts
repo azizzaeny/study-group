d575a9e049f084205b4e17e6f0a49e4992fcb806
azizzaeny <zie.azlance@gmail.com>
2020-10-21 07:17:40 +0700

    fix IUserDocument interfaces

--- backend/src/module/user/user.interface.ts
+++ backend/src/module/user/user.interface.ts
@@ -1,7 +1,9 @@
 import { Document, Model, Schema } from "mongoose";
 import { IUser, user_schema } from './../../domain/user';
 
-export interface IUserDocument extends IUser, Document { };
+export interface IUserDocument extends IUser, Document {
+
+};
 export interface IUserModel extends Model<IUserDocument> { };
 
 export const UserSchema = new Schema(user_schema());
