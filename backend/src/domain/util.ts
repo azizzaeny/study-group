e9fee1d99a9808c53f18bc82d4ab4e01fb539260
azizzaeny <zie.azlance@gmail.com>
2020-10-21 07:16:22 +0700

    add function register controller

---
+++ backend/src/domain/util.ts
@@ -0,0 +1,6 @@
+
+// TODO: should pure
+
+export function feature(name: string, schema: any) {
+  return { name: name, schema: schema }
+}
--- backend/src/module/user/user.controller.ts
+++ backend/src/module/user/user.controller.ts
@@ -10,13 +10,34 @@ import {
   Param,
 } from '@nestjs/common';
 
+import { IResponse, ResponseSuccess, ResponseError } from './../../common/dto/response.dto';
+
+import { UserDto, IUserRegistered } from './../../domain/user';
 import { UserService } from './user.service';
+import { AuthService } from './../auth/auth.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) { }
-  @Get('create')
-  async dummyCreate() {
-    return 'users/create';
+
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService
+  ) { }
+
+  @Post('register')
+  @HttpCode(HttpStatus.OK)
+  async register(@Body() userDto: UserDto): Promise<IResponse> {
+    try {
+      let created : IUserRegistered = await this.userService.registerUser(userDto);
+      let sentEmail = await this.authService.sentEmailVerification(created.profile.email);
+      if (created && sentEmail) {
+	return new ResponseSuccess('USER.REGISTRATION_SUCCESS');
+      } else {
+	return new ResponseError('USER.REGISTRATION_ERROR_EMAIL_NOT_SENT');
+      }
+    } catch (error) {
+      return new ResponseError('USER.REGISTRATION.ERROR_GENERIC', error);
+    }
+
   }
 }
