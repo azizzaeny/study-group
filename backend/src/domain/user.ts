83489fabb53149a4a0e10aa6a8d7e499af1f87bc
azizzaeny <zie.azlance@gmail.com>
2020-10-21 07:14:35 +0700

    add proper type interfaces for returned user domain

--- backend/src/domain/user.ts
+++ backend/src/domain/user.ts
@@ -1,8 +1,11 @@
+import { IAuth } from './auth';
+
 export enum UserStatus {
   PENDING,
   ACTIVE
 }
-export interface IImgProfile {
+
+export interface IProfilePic {
   url: string;
   photo: string;
   tags: Array<string>;
@@ -13,27 +16,60 @@ export interface IUser {
   full_name: string;
   phone: string;
   email: string;
-  is_valid_email?: boolean;
-  status: UserStatus;
+  status: string;
   created_at: Date;
   last_updated?: Date;
-  img_profile?: IImgProfile
+  img_profile?: IProfilePic
+}
+
+export interface IUserProfile { profile: IUser }
+
+export interface IUserAuth { auth: IAuth }
+
+export interface IUserRegistered {
+  profile: IUser,
+  auth: IAuth,
+  img_profile?: IProfilePic
+}
+
+export class UserDto {
+  readonly full_name: string;
+  readonly email: string;
+  readonly phone: string;
+  readonly password: string;
 }
 
 
-export function user_schema() {
+export const user_schema = () => {
   return {
     full_name: String,
-    phone: String,
+    phone: { type: String, default: '' },
     email: String,
-    is_valid_email: { type: Boolean, default: false },
-    status: String,
+    status: { type: String, default: 'PENDING' },
     created_at: { type: Date, default: Date.now },
     last_updated: Date,
     img_profile: {}
   }
 }
 
-export function user_feature(schema: any) {
-  return [{ name: 'User', schema: schema }];
+
+export function create_user(user: UserDto, options?: any)
+  : IUserProfile {
+  return {
+    profile: {
+      full_name: user.full_name || '',
+      phone: user.phone || '',
+      email: user.email,
+      status: options.status || 'PENDING',
+      created_at: options.created_at || new Date(),
+      img_profile: options.img_profile || {}
+    }
+  }
+
+}
+
+
+export function register_user(profile: any, auth: any, img_profile?: any | {})
+  : IUserRegistered {
+  return Object.assign({}, profile, auth, img_profile);
 }
