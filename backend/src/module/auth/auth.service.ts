0a2ed6264d39adca1b2e965f617be2539ca9f46a
azizzaeny <zie.azlance@gmail.com>
2020-10-21 10:57:18 +0700

    add more methods to the user services and controllers

--- backend/src/module/auth/auth.service.ts
+++ backend/src/module/auth/auth.service.ts
@@ -2,18 +2,35 @@ import { Injectable, HttpException, HttpStatus, HttpService } from '@nestjs/comm
 import { Model } from 'mongoose'
 import { InjectModel } from '@nestjs/mongoose';
 import { IUserDocument } from './../user/user.interface';
-import * as bcrypt from 'bcryptjs';
+import { IAuthDocument } from './auth.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel('User') private readonly userModel: Model<IUserDocument>) { }
-
-  async generatePassword(pass: string, rounds: number) {
-    return await bcrypt.hash(pass, rounds);
-  }
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<IUserDocument>,
+    @InjectModel('Auth') private readonly authModel: Model<IAuthDocument>) { }
 
   async sentEmailVerification(email: string): Promise<boolean> {
     return true;
   }
+
+  async findAll(): Promise<any> {
+    return await this.authModel.find({}).exec();
+  }
+
+  async findByEmail(email: string): Promise<any> {
+    return await this.authModel.findOne({ email: email }).exec();
+  }
+
+  async deleteByEmail(email: any): Promise<any> {
+    return await this.authModel.findOneAndDelete({ email: email });
+  }
+
+  // TODO: create proper update
+  async upsertByEmail(auth: any): Promise<any> {
+    return await this.authModel.findOneAndUpdate({ email: auth.email }, auth, { new: true, upsert: true });
+  }
+
+
 
 }
