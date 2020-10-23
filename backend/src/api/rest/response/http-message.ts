acbc017c6e0aef5090abe236a3bcc40722954b2f
azizzaeny <zie.azlance@gmail.com>
2020-10-23 11:38:14 +0700

    use fix interfaces for response

---
+++ backend/src/api/rest/response/http-message.ts
@@ -0,0 +1,55 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export interface IHttpMessage {
+  message: string,
+  docs_url?: string,
+};
+
+export type IResponseS<T> = {
+  message: string,
+  success: true;
+  value: T,
+}
+
+export type IResponseF<T> = {
+  success: false,
+  type: string,
+  error: T,
+}
+
+export function success<T>(msg: string, value?: T, logging?: boolean): IResponseS<T> {
+  if (!logging) {
+    try {
+      let offR = JSON.parse(JSON.stringify(value));
+      if (offR && offR.token) offR.token = "*****";
+      console.log(new Date().toString() + '-[IResponseS]:' + JSON.stringify(offR));
+    } catch (error) { }
+  }
+  return <IResponseS<T>>{
+    success: true,
+    message: msg,
+    value: value
+  }
+}
+
+export function failure<T>(msg: string, value?: T): IResponseF<T> {
+  console.warn(new Date().toString() + '- [IResponseF]:' + msg + (value ? ' - ' + JSON.stringify(value) : ''));
+  return <IResponseF<T>>{
+    success: false,
+    type: msg,
+    error: value
+  }
+}
+export function exception(msg: string, status?:any): HttpException {
+  return new HttpException(msg, status);
+}
+
+export function refer_docs(url: string): IHttpMessage {
+  return {
+    message: 'To use this services, please read documentation ',
+    docs_url: url
+  }
+}
+
+
+export type IResponse<T> = IResponseF<T> | IResponseS<T>| HttpException;
