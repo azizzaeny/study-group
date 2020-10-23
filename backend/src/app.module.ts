4446a5516ac15533ba7c8ae8edd7756fa7ac64a9
azizzaeny <zie.azlance@gmail.com>
2020-10-23 11:30:45 +0700

    use config module as global

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,27 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './module/user/user.module';
+
+import { RestModule } from 'src/api/rest/rest.module';
+import { ProviderModule } from 'src/providers/provider.module';
 
 import config from './config';
-import { db_url } from './domain/db';
-
-const conf = config();
 
 const configModule = ConfigModule.forRoot({
+  isGlobal: true,
   load: [config]
 });
-
-const mongooseModule = MongooseModule.forRoot(db_url(conf.db));
 
 @Module({
   imports: [
     configModule,
-    mongooseModule,
-    UserModule
+    ProviderModule,
+    RestModule,
   ],
   controllers: [AppController],
   providers: [AppService]
