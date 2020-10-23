535041405d7682ffbb87bc39454386bc4d949bb1
azizzaeny <zie.azlance@gmail.com>
2020-10-23 11:39:54 +0700

    route initiate mongoose connection to db-connector module

---
+++ backend/src/providers/db-connector/db-connector.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from 'src/config';
+import { db_url } from './db';
+const conf = config();
+const mongooseModule = MongooseModule.forRoot(db_url(conf.db));
+
+@Module({
+  imports: [mongooseModule],
+  providers: [],
+  exports: [mongooseModule]
+})
+export class DbConnectorModule { }
