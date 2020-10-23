cbd422481cd6797b04de474b9d07fbe9fcfffe19
azizzaeny <zie.azlance@gmail.com>
2020-10-23 11:43:02 +0700

    make jwt services injectable

---
+++ backend/src/providers/auth/services/jwt.service.ts
@@ -0,0 +1,34 @@
+import * as jwt from 'jsonwebtoken';
+import { Injectable } from '@nestjs/common';                                                          
+
+import { ConfigService } from '@nestjs/config';
+
+import { InjectModel } from '@nestjs/mongoose';
+
+import { IUserModel } from 'src/providers/user/domain/entity';
+
+@Injectable()                                                                                        
+export class JwtService {
+  constructor(private readonly configService: ConfigService,
+	      @InjectModel('User') private readonly userModel: IUserModel){}
+
+  async createToken(email, roles){
+    let jwtConfig = this.configService.get('jwt');
+    let expiresIn = jwtConfig.expiresIn;
+    let salt      = jwtConfig.salt;
+    let user_sign      = {email: email, roles: roles };
+    let token = jwt.sign(user_sign, salt, {expiresIn});
+    return {
+      expires_in: expiresIn,
+      access_token: token
+    }
+  }
+
+  async verifyUser(signedEmail){
+    const record =  await this.userModel.findOne({email: signedEmail});
+    if(record){
+      return record;
+    }
+    return null;   
+  }
+}
