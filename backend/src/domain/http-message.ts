7d56ac8dc8baf8d1b41e3d003beb21bdb4424745
azizzaeny <zie.azlance@gmail.com>
2020-10-23 11:32:21 +0700

    remove combined domain

--- backend/src/domain/http-message.ts
+++
@@ -1,9 +0,0 @@
-export interface IHttpMessage {
-  msg: string
-};
-
-export function refer_docs(url: string): IHttpMessage {
-  return {
-    msg: 'To use this services, please read documentation at ' + url
-  }
-}
--- backend/src/domain/util.ts
+++
@@ -1,6 +0,0 @@
-
-// TODO: should pure
-
-export function feature(name: string, schema: any) {
-  return { name: name, schema: schema }
-}
