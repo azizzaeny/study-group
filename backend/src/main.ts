b9494026f7891bff76a277654c67462e2e4cea38
azizzaeny <zie.azlance@gmail.com>
2020-10-23 11:33:10 +0700

    use global prefix and exception filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { DefaultExceptionFilter } from './common/filters/default-exception.filter';
+import { DefaultExceptionFilter } from 'src/api/rest/filters/default-exception.filter';
 import * as bodyParser from 'body-parser';
+import * as helmet from 'helmet';
 
 import { AppModule } from './app.module'
 import config from './config';
@@ -9,16 +10,19 @@ import config from './config';
 const conf = config();
 
 async function bootstrap() {
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use(bodyParser.json({ limit: '4mb' }));
-  app.use(bodyParser.urlencoded({ limit: '4mb', extended: true }));
+  app.use(helmet());
+  app.use(bodyParser.json({ limit: conf.json.limit }));
+  app.use(bodyParser.urlencoded(conf.json));
 
-
-
+  app.setGlobalPrefix('api');
   app.useGlobalFilters(new DefaultExceptionFilter());
 
+
   await app.listen(conf.http.port);
+
 
   console.log('Http Application started at port ', conf.http.port);
 
